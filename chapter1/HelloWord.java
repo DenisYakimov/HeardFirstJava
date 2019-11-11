1090bcec793ea6fa516edf1c3f8ea6ad9183c2d4
Denis Yakimov <55536597+DenisYakimov@users.noreply.github.com>
2019-11-11 14:39:06 +0300

    Create HelloWord.java

---
+++ chapter1/HelloWord.java
@@ -0,0 +1,15 @@
+package chaper1;
+
+/**
+ * java. My first program
+ *
+ * @author Denis Yakimov
+ * @version Oct 02.19
+ */
+
+public class HelloWord
+{    public static void main(String[] args) {
+
+    System.out.println("Hello world!");
+}
+}
