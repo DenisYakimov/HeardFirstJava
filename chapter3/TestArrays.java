b07b1886b53ab09393fdbe0f8424c667bc25e718
Denis Yakimov <55536597+DenisYakimov@users.noreply.github.com>
2019-12-10 17:58:24 +0300

    Update TestArrays.java
    Комментарии

--- chapter3/TestArrays.java
+++ chapter3/TestArrays.java
@@ -1,21 +1,21 @@
 public class TestArrays {
     public static void main(String[] args) {
-        String [] islands = new String[4];
+        String [] islands = new String[4]; // создаем массив
 
-        islands[0] = "Бермуды";
+        islands[0] = "Бермуды";     // заполняем массив именами островом
         islands[1] = "Фиджи";
         islands[2] = "Азорские острова";
         islands[3] = "Косумель";
 
-        int[] index = new int[4];
+        int[] index = new int[4]; // создаем массив последовательности отображения на экране и присваиваем значение
         index[0] = 1;
         index[1] = 3;
         index[2] = 0;
         index[3] = 2;
 
-        int y = 0;
+        int y = 0;  // вывод на экран имен островов в соответсвии с последовательностью массива index;
         int ref;
-         while (y < 4 ) {
+        while (y < 4 ) {
             ref = index[y];
             System.out.println(islands[ref]);
             y = y + 1;
