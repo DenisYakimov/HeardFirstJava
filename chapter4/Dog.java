59f613605dce1780c29d8a8b098b579e277b6101
Denis Yakimov <55536597+DenisYakimov@users.noreply.github.com>
2019-12-10 19:53:37 +0300

    Update Dog.java

--- chapter4/Dog.java
+++ chapter4/Dog.java
@@ -1,22 +1,27 @@
-
 public class Dog {
     int size;
     String name;
 
     public Dog(int size) {  // шаблон, с вынесенным размером собаки
         this.size = size;
-        bark();
+        bark(3);
     }
 
-    void bark() {                   // метод голос, с условиями
-       if (size > 60) {
-           System.out.println("Гав Гав");
-       } else if( size > 14)
-           System.out.println( "Вуф Вуф");
-       else System.out.println( "Тяф Тяф");
-
+    void bark(int numOfBarks) {                   // метод голос, с обозначением количества повторений
+        while (numOfBarks > 0) {
+            barkIf();
+            numOfBarks--;
         }
     }
+
+    private void barkIf() {                 // метод условий вызова голоса, в зависимости от размера собаки 
+        if (size > 60) {
+            System.out.println("Гав Гав");
+        } else if (size > 14) System.out.println("Вуф Вуф");
+        else System.out.println("Тяф Тяф");
+    }
+}
+
 
 class DogTestDrive {                        // проверка метода голос, в зависимости от размера собаки
     public static void main(String[] args) {
