0a81142e111f0dd8c16fb292f3ca128de467c683
Denis Yakimov <55536597+DenisYakimov@users.noreply.github.com>
2019-12-10 14:55:20 +0300

    Update DrumKit.java

--- chapter2/DrumKit.java
+++ chapter2/DrumKit.java
@@ -1,28 +1,27 @@
-class DrumKit {
+public class DrumKit {
     boolean topHat = true;
     boolean snare = true;
+
+    void playSnare() {
+        System.out.println("бах бах ба-бах");
+    }
 
     void playTopHat() {
         System.out.println("динь динь ди-динь");
     }
 
-    void playSnare() {
-        System.out.println("бах бах ба-бах");
-    }
 }
 
-  class DrumKitTestDrive {
-        public static void main(String[] args) {
+class DrumKitTestDrive {
+    public static void main(String[] args) {
+        DrumKit d = new DrumKit();
 
-            DrumKit d = new DrumKit();
+        d.playSnare();
+        d.snare = false;
 
+        if (d.snare == true) {
             d.playSnare();
-            d.snare = false;
-
-            d.playTopHat();
-
-            if (d.snare == true) {
-                d.playSnare();
-            }
         }
+        d.playTopHat();
     }
+}
